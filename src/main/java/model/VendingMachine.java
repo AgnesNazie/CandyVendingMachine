c368575b1aea51d8f710eeb8f7603f4196c7402c
AgnesNazie <cheagnesmbo@gmail.com>
2025-04-02 18:41:17 +0200

    created an interface vending machine implements

--- src/main/java/model/VendingMachine.java
+++ src/main/java/model/VendingMachine.java
@@ -3,11 +3,22 @@ package model;
 import java.util.List;
 
 public interface VendingMachine {
+    // adds money to the deposit pool
     void addCurrency (int amount);
+
+    //returns balance in the deposit pool
     int getBalance();
+
+    //returns the products if balance is enough in the deposit pool
     Product request(int id);
+
+    // ends the session after buying and returns balance
     int endSession ();
+
+    //gives the description of the product based on id
     String getDescription(int id);
+
+    //gives list of all products
     List<String> getProducts();
 
 }
